3ed4e324c501fac81116e3a1506f1f62b34470fd
salvism <salviai@code.edu.az>
2023-01-25 02:23:25 +0400

    Method practice

---
+++ Homework6/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Xml;
+
+namespace Homework6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "salvi";
+            Capitalize(ref name);
+            Console.WriteLine(name);
+
+            string nameInput;
+            do
+            {
+                Console.Write("Enter your name: ");
+                nameInput = Console.ReadLine();
+
+            } while (IsName(nameInput) == false);
+
+            //console-da email deyeri istenilsin. Eger deyerde @ xarakteri yoxdursa yeniden istenilsin
+            string email;
+            do
+            {
+                Console.Write("Enter E-mail: ");
+                email = Console.ReadLine();
+            } while (!email.Contains('@'));
+
+            Console.WriteLine(GetDomain(email));
+
+            name = "Salvi";
+            Console.WriteLine(IsName(name));
+
+            string[] fullnames = { "   Salvi   Ismayilzada  ", "Bahar Behbudova"};
+            var names = MakeNames(fullnames);
+            
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            string[] array = { "hello", "world", "harry", "potter", "hello", "potter" };
+
+            var newArray = MakeUniqueArray(array);
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.WriteLine(newArray[i]);
+            }
+
+        }
+
+        //Verilmis yazini bas herfi boyuk digerleri kicik halda qaytaran metod
+        static void Capitalize(ref string str)
+        {
+            str = char.ToUpper(str[0]) + str.Substring(1).ToLower();
+        }
+
+       //Verilmis yazinin ad olub olmadigini tapan metod
+        static bool IsName(string str)
+        {
+            if(string.IsNullOrWhiteSpace(str))
+                return false;
+
+            if (!char.IsUpper(str[0]))
+                return false;
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (!char.IsLower(str[i]))
+                    return false;
+            }
+
+            return true;
+        }
+      
+        //Verilmis fullname siyahisindan names siyahisi duzeldib qaytaran metod
+        static string[] MakeNames(string[] arr)
+        {
+            string[] newArr = new string[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var fullname = arr[i].TrimStart();
+                var name = arr[i].TrimStart().Substring(0, fullname.IndexOf(' '));
+                newArr[i] = name;
+            }
+
+            return newArr;
+        }
+
+        //Verilmis string arrayinin icerisindeki tekrarlanmayan deyerlerden ibaret
+        //yeni bir array qaytaran metod
+        static string[] MakeUniqueArray(string[] arr)
+        {
+            string[] newArr = new string[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Array.IndexOf(newArr, arr[i]) == -1)
+                {
+                    Array.Resize(ref newArr, newArr.Length + 1);
+                    newArr[newArr.Length - 1] = arr[i];
+                }
+            }
+            return newArr;
+        }
+
+        static string GetDomain(string str)
+        {
+           return str.Substring(str.IndexOf('@') + 1);
+        }
+    }
+}
---
+++ New Tasks/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace New_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "P231";
+            Console.WriteLine(IsDigit(word));
+
+            string fullname = "Harry potter";
+            Console.WriteLine(IsFullname(fullname));
+
+            int[] numbers = { 1, 2, 3, 4, 4, 6, 6 };
+            var newNumbers = MakeUniqueNumbers(numbers);
+            for (int i = 0; i < newNumbers.Length; i++)
+            {
+                Console.WriteLine(newNumbers[i]);
+            }
+
+            string[] emails = { "salviai@code.edu.az", "salvi.ismayilzada@gmail.com"};
+            MakeDomains(ref emails);
+            for (int i = 0; i < emails.Length; i++)
+            {
+                Console.WriteLine(emails[i]);
+            }
+
+            string sentences = "Hello world. How are you. Everything is ok.";
+            var count = CountOfSentences(sentences);
+            Console.WriteLine(count);
+        }
+
+        //- Verilmiş yazıda rəqəm olub olmadığını tapan metod
+        static bool IsDigit(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        //- Verilmiş yazının fullname olub olmadığını tapan metod
+        //(fullname olması üçün iki sozdən ibarət olmalıdır
+        //və hər bir söz böyük hərflə başlayıb kiçik hərflərlə davam etməlidir)
+        static bool IsName(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            if (!char.IsUpper(str[0]))
+                return false;
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (!char.IsLower(str[i]))
+                    return false;
+            }
+            return true;
+        }
+        static bool IsFullname(string str)
+        {
+            string[] newStr = str.Split(' ');
+
+            var name = newStr[0];
+            var surname = newStr[1];
+
+            if(IsName(name) && IsName(surname))
+                return true;
+            return false;
+        }
+
+        //- Verilmiş ədədlər siyahısından yeni bir array düzəldib qaytaran metod.
+        //Yeni arrayə elementlər təkrarlanmamaq şərti ilə yerləşdirilsin.
+        static int[] MakeUniqueNumbers(int[] arr)
+        {
+            int[] newArr = new int[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Array.IndexOf(newArr, arr[i]) == -1)
+                {
+                    Array.Resize(ref newArr, newArr.Length + 1);
+                    newArr[newArr.Length - 1] = arr[i];
+                }
+            }
+            return newArr;
+        }
+
+        // - Verilmiş email-lər siyahısından domainlər siyahısı düzəldən metod.
+        static void MakeDomains(ref string[] str)
+        {
+            string[] domains = new string[0];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                Array.Resize(ref domains, domains.Length + 1);
+                domains[domains.Length - 1] = str[i].Substring(str[i].IndexOf('@') + 1);
+            }
+
+            str = domains;
+        }
+
+        //- Verilmiş yazının içindəki cümlələrin sayını tapan metod.
+        static int CountOfSentences(string str)
+        {
+            int count = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.ToString(str[i]).Contains('.'))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
